97259c4c857f687d109fbc1bc6f16dc640e65725
zuxfoucault <zuxfoucault@gmail.com>
2016-09-22 11:37:38 +0800

    make contrast of ramping smoothier

--- targetImage.m
+++ targetImage.m
@@ -19,26 +19,45 @@ function targetImage(initSetting, experimentPars, cfsPars)
 	nTrials = 1;
 	flashCount = 0;
 	alpha = 0.1; %initSetting.contrastStiInit
-	ramping = 0.01;
+	ramping = 0.001;
 	i = 0;
-	while i < nTrials, % iteration of trial
+	% preparation
+	[object,xCenter, yCenter, color] = ...
 		drawMondrians(initSetting, experimentPars, cfsPars);
+	Screen('DrawTexture', initSetting.windowPtr, tex, [], rectImage, [], [], alpha);
+	drawHolderFrameCfs(initSetting, experimentPars, cfsPars);
+	[initSetting.tvbl initSetting.sot] = Screen('Flip', initSetting.windowPtr, initSetting.sot + experimentPars.duration - initSetting.ifi*0.5);
+	tmp1 = initSetting.sot;
+	tmp2 = initSetting.sot;
+
+	while i < nTrials, % iteration of trial
+		drawMondriansFixPatch(initSetting, object, xCenter, yCenter, color);
 		Screen('DrawTexture', initSetting.windowPtr, tex, [], rectImage, [], [], alpha);
 		drawHolderFrameCfs(initSetting, experimentPars, cfsPars);
-		[initSetting.tvbl initSetting.sot] = Screen('Flip', initSetting.windowPtr, initSetting.sot + experimentPars.duration - initSetting.ifi*0.5);
+		[initSetting.tvbl initSetting.sot] = Screen('Flip', initSetting.windowPtr);
 
-		if alpha <= 1,
-			alpha = alpha + ramping;
-		end;
 
-		if (experimentPars.durationTarget - cfsPars.mondFlashDur*flashCount)/cfsPars.mondFlashDur >= 1
-			experimentPars.duration = cfsPars.mondFlashDur;
-			flashCount = flashCount + 1;
-		else,
-			experimentPars.duration = mod(experimentPars.durationTarget, cfsPars.mondFlashDur);
-			flashCount = 0;
+		if (initSetting.sot - tmp1)/experimentPars.durationTarget < 1,
+			if (initSetting.sot - tmp2)/cfsPars.mondFlashDur >= 1
+				[object,xCenter, yCenter, color] = ...
+					drawMondrians(initSetting, experimentPars, cfsPars);
+				tmp2 = initSetting.sot;
+			end
+
+
+			if alpha <= 1, % Set contrast max
+				alpha = alpha + ramping;
+			end;
+
+		else
+			[object,xCenter, yCenter, color] = ...
+				drawMondrians(initSetting, experimentPars, cfsPars);
+			
 			i = i + 1;
+			alpha = 1; % reset alpha
+			tmp1 = initSetting.sot;
 		end;
+
 	end;
 
 	Screen('DrawingFinished', initSetting.windowPtr);
